33c4bebf99ad17a78f45fff9de6b17b774d74bd8
thyagovis <thyagosilva321123@gmail.com>
2026-01-02 14:49:08 -0300

    consumo top 20

--- script.js
+++ script.js
@@ -1,16 +1,46 @@
-const display = document.getElementById("display")
+const display = document.getElementById("view")
 
-function CriarCard(Nome){
+function CriarRow(nome, simbolo, logo, preco, mudancas, cap, vol){
 
-    const card = document.createElement("div")
-    const Nome_crypto = document.createElement("h2")
+    const row = document.createElement("tr")
 
-    card.className = "card"
-    Nome_crypto.className = "name"
+    const name = document.createElement("h3")
+    const symbol = document.createElement("p")
+    const logo_text = document.createElement('td')
+    const price = document.createElement("td")
+    const changes = document.createElement("td")
+    const market = document.createElement("td")
+    const volume = document.createElement("td")
+    const div_name = document.createElement('div')
 
-    Nome_crypto.textContent = `${Nome}`
-    card.appendChild(Nome_crypto)
-    display.appendChild(card)
+    const logo_image = document.createElement('img')
+    const div_text = document.createElement('div')
+
+    div_name.className = 'logo-nome'
+    
+    div_name.appendChild(logo_image)
+    div_name.appendChild(div_text)
+    
+    div_text.appendChild(name)
+    div_text.appendChild(symbol)
+
+    logo_text.appendChild(div_name)
+    
+    logo_image.src = logo
+    symbol.textContent = `${simbolo}`
+    name.textContent = `${nome}`
+    price.textContent = `${preco}`
+    changes.textContent = `${mudancas}`
+    market.textContent = `${cap}`
+    volume.textContent = `${vol}`
+    
+    row.append(logo_text)
+    row.appendChild(price)
+    row.appendChild(changes)
+    row.appendChild(market)
+    row.appendChild(volume)
+
+    display.appendChild(row)
 }
 
 function FetchApi(){
@@ -30,11 +60,23 @@ async function Consultar(){
     )
 
     for(let i = 0; i < filtered_result.length; i++){
-
-        console.log(filtered_result[i].name)
+    
+        CriarRow(
+            filtered_result[i].name,
+            filtered_result[i].symbol,
+            `https://static.coinpaprika.com/coin/${filtered_result[i].id}/logo.png`,
+            filtered_result[i].quotes.USD.price,
+            filtered_result[i].quotes.USD.percent_change_24h,
+            filtered_result[i].quotes.USD.market_cap,
+            filtered_result[i].quotes.USD.volume_24h
+        )
 
     }
+
+
 
 /*     display.textContent = `${JSON.display(filtered_result, undefined, 2)}`
  */
 }
+
+Consultar()
